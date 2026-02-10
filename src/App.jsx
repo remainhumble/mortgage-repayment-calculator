b18acd0e64204bbdea48017d40bf3e348368bbf5
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-02-10 16:00:14 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Calculator from './Calculator'
+import Results from './Results'
+
+
+const App = () => {
+  return (
+    <>
+      <Calculator />
+      <Results />
+    </>
+  )
+}
+
+export default App
---
+++ src/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Calculator = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Mortgage Calculator</h1>
+            <p>Clear All</p>
+        </div>
+    )
+}
+
+export default Calculator
