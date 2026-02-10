0002a2186f19f224520f2295e4712b4286b512f0
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-02-10 21:20:47 +1300

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react(), tailwindcss()] });
+export default defineConfig({ plugins: [react(), tailwindcss()],
+    base: "/mortgage-repayment-calculator-main/",
+ });
