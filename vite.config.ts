dd9c8b991a2671aa795580f69a4508ee68029529
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-02-10 21:45:41 +1300

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-// Set `base` to your GitHub Pages repository subpath so built assets load correctly.
-export default defineConfig({
-    base: '/mortgage-repayment-calculator/',
-    plugins: [react(), tailwindcss()],
+export default defineConfig({ plugins: [react(), tailwindcss()],
+    base: "/mortgage-repayment-calculator-main/",
  });
