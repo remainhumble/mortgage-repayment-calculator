433fbcc5606968a3a10a2ddd94242b6e80eea5f0
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-02-16 18:44:07 +1300

    addons

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -6,12 +6,25 @@ const Calculator = () => {
         <div>
             <h1 className='text-4xl font-bold'>Mortgage Calculator</h1>
             <p><u>Clear All</u></p>
+
             <label for="mortgage-amount">Mortgage Amount</label><br></br>
-            <input type="number" id="mortgage-amount" className='number-input' /><br></br>
+            <div className="left-addon">
+                <div className='addon'>£</div>
+                <input type="number" id="mortgage-amount" className='number-input' /><br></br>
+            </div>
+
             <label for="mortgage-term">Mortgage Term</label><br></br>
-            <input type="number" id="mortgage-term" className='number-input' /><br></br>
+            <div className="right-addon">
+                <input type="number" id="mortgage-term" className='number-input' />
+                <div className='addon'>years</div>
+            </div>
+            <br></br>
             <label for="interest-rate">Interest Rate</label><br></br>
-            <input type="number" id="interest-rate" className='number-input' /><br></br>
+            <div className="right-addon">
+                <input type="number" id="interest-rate" className='number-input' />
+                <div className='addon'>%</div>
+            </div>
+            <br></br>
             <div className='mortgage-type'>
                 <label for="mortgage-type">Mortgage Type</label><br></br>
                 <div className='radio'>
