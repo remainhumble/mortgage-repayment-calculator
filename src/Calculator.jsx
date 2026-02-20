de1c7c3e0851040da748071b754f6e7ed467c0c4
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2026-02-20 21:38:38 +1300

    some input styling adjustment

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,45 +3,47 @@ import './index.css';
 
 const Calculator = () => {
     return (
-        <div className='calculator'>
-            <h1 className='text-3xl font-bold'>Mortgage Calculator</h1>
-            <p className='cursor-pointer'><u>Clear All</u></p>
-            <div className='inputs'>
-                <label for="mortgage-amount">Mortgage Amount</label><br></br>
-                <div className="left-addon">
-                    <div className='addon'>£</div>
-                    <input type="number" id="mortgage-amount" className='number-input' /><br></br>
-                </div>
+        <>
+            <div className='calculator bg-white rounded-bl-[25px] rounded-tl-[25px]'>
+                <h1 className='text-3xl font-bold'>Mortgage Calculator</h1>
+                <p className='cursor-pointer'><u>Clear All</u></p>
+                <div className='inputs'>
+                    <label for="mortgage-amount">Mortgage Amount</label><br></br>
+                    <div className="left-addon">
+                        <div className='addon'>£</div>
+                        <input type="number" id="mortgage-amount" className='number-input' /><br></br>
+                    </div>
+                    <br />
+                    <label for="mortgage-term">Mortgage Term</label><br></br>
+                    <div className="right-addon">
+                        <input type="number" id="mortgage-term" className='number-input' />
+                        <div className='addon'>years</div>
+                    </div>
+                    <br></br>
+                    <label for="interest-rate">Interest Rate</label><br></br>
+                    <div className="right-addon">
+                        <input type="number" id="interest-rate" className='number-input' />
+                        <div className='addon'>%</div>
+                    </div>
+                    <br></br>
 
-                <label for="mortgage-term">Mortgage Term</label><br></br>
-                <div className="right-addon">
-                    <input type="number" id="mortgage-term" className='number-input' />
-                    <div className='addon'>years</div>
-                </div>
-                <br></br>
-                <label for="interest-rate">Interest Rate</label><br></br>
-                <div className="right-addon">
-                    <input type="number" id="interest-rate" className='number-input' />
-                    <div className='addon'>%</div>
-                </div>
-                <br></br>
-
-                <fieldset className='mortgage-type flex flex-col gap-2.5'>
-                    <legend for="mortgage-type">Mortgage Type</legend><br></br>
-                    <label className='radio font-bold'>
-                        <input type="radio" name="mortgage-type" value="repayment" id="repayment" />
-                        Repayment
-                    </label>
-                    <label className='radio font-bold'>
-                        <input type="radio" name="mortgage-type" value="interest-only" id="interest-only" />
-                        Interest Only
-                    </label>
-                </fieldset>
-                <div className='flex gap-2.5 mt-5'>
-                    <button className='flex calculate-btn font-bold w-full justify-center'><img src="./assets/images/icon-calculator.svg" alt="Calculate" />Calculate Repayments</button>
+                    <fieldset className='mortgage-type flex flex-col gap-2.5'>
+                        <legend for="mortgage-type">Mortgage Type</legend><br></br>
+                        <label className='radio font-bold'>
+                            <input type="radio" name="mortgage-type" value="repayment" id="repayment" />
+                            Repayment
+                        </label>
+                        <label className='radio font-bold'>
+                            <input type="radio" name="mortgage-type" value="interest-only" id="interest-only" />
+                            Interest Only
+                        </label>
+                    </fieldset>
+                    <div className='flex gap-2.5 mt-5'>
+                        <button className='cursor-pointer flex calculate-btn font-bold justify-center'><img src="./assets/images/icon-calculator.svg" alt="Calculate" />Calculate Repayments</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/Results.jsx
+++ src/Results.jsx
@@ -3,13 +3,13 @@ import './index.css';
 
 const Results = () => {
     return (
-
-            <div className="bg-[#122f3f] flex flex-col justify-center items-center max-w-fit rounded-lg">
-                <img src="./assets/images/illustration-empty.svg" alt="Mortgage Results" />
+        <>
+            <div className="bg-[#122f3f] flex flex-col justify-center items-center max-w-fit rounded-bl-[50px]  rounded-br-[25px]  rounded-tr-[25px] ">
+                <img src="./assets/images/illustration-empty.svg" alt="Mortgage Results" className='w-64' />
                 <h2 className='text-white text-2xl font-bold'>Results shown here</h2>
                 <p className='text-slate-300 text-sm mt-2 text-center'>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
             </div>
-
+        </>
     )
 }
 
